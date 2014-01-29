96d9ada5ef0b748cc6fa0a9917ad9f8cd19fe967
David Huang <magichz@outlook.com>
2014-01-29 22:13:06 +1100

    project created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Acquisition::Application.config.secret_key_base = 'c5766cde37efd9089caaa94e242efb1ee164b7a648a35bf9f704d91508f8acfcc727b0f2111367191a49a1c781e7ceb8ee40a1671aea32bec0d815c3d8cb6565'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Acquisition::Application.config.session_store :cookie_store, key: '_Acquisition_session'
